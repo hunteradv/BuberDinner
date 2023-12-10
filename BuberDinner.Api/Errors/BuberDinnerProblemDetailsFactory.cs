d4b4a311d46908f13a207145dc2c2150abf5f9a8
Gustavo Henrique <gustavo.depaulo@appsistemas.com.br>
2023-12-10 13:35:14 -0300

    Ajustando Details Factory

--- BuberDinner.Api/Errors/BuberDinnerProblemDetailsFactory.cs
+++ BuberDinner.Api/Errors/BuberDinnerProblemDetailsFactory.cs
@@ -38,7 +38,27 @@ public class BuberDinnerProblemDetailsFactory : ProblemDetailsFactory
         ModelStateDictionary modelStateDictionary, int? statusCode = null, string? title = null, string? type = null,
         string? detail = null, string? instance = null)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(modelStateDictionary);
+
+        statusCode ??= 400;
+
+        var problemDetails = new ValidationProblemDetails(modelStateDictionary)
+        {
+            Status = statusCode,
+            Type = type,
+            Detail = detail,
+            Instance = instance,
+        };
+
+        if (title != null)
+        {
+            // For validation problem details, don't overwrite the default title with null.
+            problemDetails.Title = title;
+        }
+
+        ApplyProblemDetailsDefaults(httpContext, problemDetails, statusCode.Value);
+
+        return problemDetails;
     }
 
     private void ApplyProblemDetailsDefaults(HttpContext httpContext, ProblemDetails problemDetails, int statusCode)
