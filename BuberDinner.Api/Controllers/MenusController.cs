caf0908aa1b7e9ce4094493620ea1f82bc75014e
Gustavo <gustavopaulody@gmail.com>
2024-03-24 14:11:33 -0300

    iniciando novo controller

---
+++ BuberDinner.Api/Controllers/MenusController.cs
@@ -0,0 +1,27 @@
+﻿using BuberDinner.Application.Menus.Commands.CreateMenu;
+using BuberDinner.Contracts.Menus;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("hosts/{hostId}/menus")]
+    public class MenusController : ApiController
+    {
+        private readonly IMapper _mapper;
+
+        public MenusController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CreateMenu(CreateMenuRequest request, string hostId)
+        {
+            //var command = _mapper.Map<CreateMenuCommand>((request, ))
+
+            return Ok(request);
+        }
+    }
+}
---
+++ BuberDinner.Application/Menus/Commands/CreateMenu/CreateMenuCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuberDinner.Domain.Menu;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Menus.Commands.CreateMenu
+{
+    public class CreateMenuCommandHandler : IRequestHandler<CreateMenuCommand, ErrorOr<Menu>>
+    {
+        public Task<ErrorOr<Menu>> Handle(CreateMenuCommand request, CancellationToken cancellationToken)
+        {
+            //create menu
+
+            //persist menu
+
+            //return menu
+            return default!;
+        }
+    }
+}
